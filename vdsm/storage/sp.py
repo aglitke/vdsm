d91fb3f92a8ddeb350e2e6c5ac2ce2f69fd6089d
Adam Litke <alitke@redhat.com>
2014-01-08 09:59:33 -0500

    avoid redundant domain produce() in createStoragePool
    As the domains in hsm.createStoragePool are going to
    be accessed much later, and may loose validity till then.
    Just produce from cache and check the validity when actully use it.
    
    Change-Id: If1c236aa3043068ca8f3f376cb340e986cd484bb
    Signed-off-by: Adam Litke <alitke@redhat.com>

--- vdsm/storage/sp.py
+++ vdsm/storage/sp.py
@@ -600,12 +600,15 @@ class StoragePool(Securable):
             raise se.InvalidParameterException("masterDomain", msdUUID)
 
         # Check the domains before pool creation
+        domains = []
+        msdVersion = None
         for sdUUID in domList:
             try:
                 domain = sdCache.produce(sdUUID)
                 domain.validate()
                 if sdUUID == msdUUID:
                     msd = domain
+                    msdVersion = msd.getVersion()
             except se.StorageException:
                 self.log.error("Unexpected error", exc_info=True)
                 raise se.StorageDomainAccessError(sdUUID)
@@ -617,6 +620,13 @@ class StoragePool(Securable):
                 # Non ISO domains have only 1 pool
                 if len(spUUIDs) > 0:
                     raise se.StorageDomainAlreadyAttached(spUUIDs[0], sdUUID)
+            domains.append(domain)
+
+        for domain in domains:
+            if domain.isData() and (domain.getVersion() != msdVersion):
+                raise se.MixedSDVersionError(domain.sdUUID,
+                                             domain.getVersion(), msd.sdUUID,
+                                             msdVersion)
 
         fileUtils.createdir(self.poolPath)
         self._acquireTemporaryClusterLock(msdUUID, leaseParams)
