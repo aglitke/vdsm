9603453300e4116e4b885f26944687b1810bd8c7
Adam Litke <alitke@redhat.com>
2015-07-10 12:56:55 -0400

    sdm: Add decorators for sdm-only functions and their callers
    Change-Id: Iaf736144e5640519851dc9175b5f17539d0ce23e
    Signed-off-by: Adam Litke <alitke@redhat.com>

---
+++ tests/sdm_tests.py
@@ -0,0 +1,43 @@
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from testlib import VdsmTestCase
+
+from storage.sdmprotect import sdm_verb, require_sdm, SDMFunctionNotCallable
+from storage.threadLocal import vars
+
+
+class RequireSDMTests(VdsmTestCase):
+    def setUp(self):
+        try:
+            delattr(vars, '__sdm__')
+        except AttributeError:
+            pass
+
+    @sdm_verb
+    def test_allowed(self):
+        self.assertTrue(self.sdm_fn())
+
+    def test_denied(self):
+        self.assertRaises(SDMFunctionNotCallable, self.sdm_fn)
+
+    @require_sdm
+    def sdm_fn(self):
+        return True
