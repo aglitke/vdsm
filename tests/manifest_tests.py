7b8349ea27dd7389efa3d9fbdd880076a76958eb
Adam Litke <alitke@redhat.com>
2015-07-28 10:07:35 -0400

    [TESTS] StorageDomainManifest: move extend and extendVolume
    Change-Id: I427cdb1662be7e8b9e090e5014e17876e3595462
    Signed-off-by: Adam Litke <alitke@redhat.com>

--- tests/manifest_tests.py
+++ tests/manifest_tests.py
@@ -23,12 +23,16 @@ import uuid
 from testlib import VdsmTestCase, namedTemporaryDir, make_file
 from monkeypatch import MonkeyPatchScope
 from storagefakelib import FakeLVM
-from storagetestlib import make_filesd_manifest, make_blocksd_manifest, \
-    make_file_volume, make_vg
+from storagetestlib import (make_filesd_manifest, make_blocksd_manifest,
+                            make_file_volume, make_vg, get_random_devices,
+                            FakeMetadata)
 
-from storage import sd, blockSD
+from storage import sd, blockSD, multipath
+from storage import storage_exception as se
 
-VOLSIZE = 1048576
+MB = 1 << 20
+GB = 1 << 30
+VOLSIZE = 1 * MB
 
 
 class FileManifestTests(VdsmTestCase):
@@ -73,6 +77,8 @@ class FileManifestTests(VdsmTestCase):
 
 
 class BlockManifestTests(VdsmTestCase):
+    MINIMUM_METADATA_SIZE = 512 * MB
+    PV_UNUSABLE_SIZE = 384 * MB
 
     def test_getreaddelay(self):
         with namedTemporaryDir() as tmpdir:
@@ -111,7 +117,7 @@ class BlockManifestTests(VdsmTestCase):
 
     def test_getmetaparam(self):
         with namedTemporaryDir() as tmpdir:
-            metadata = {}
+            metadata = FakeMetadata()
             manifest = make_blocksd_manifest(tmpdir, metadata)
             metadata[sd.DMDK_SDUUID] = manifest.sdUUID
             self.assertEquals(manifest.sdUUID,
@@ -130,3 +136,201 @@ class BlockManifestTests(VdsmTestCase):
             manifest = make_blocksd_manifest(tmpdir, metadata)
             self.assertEquals(2048, manifest.logBlkSize)
             self.assertEquals(1024, manifest.phyBlkSize)
+
+    def test_metasize(self):
+        """
+        Test the check for enough free space to accommodate the metadata LV
+        """
+        with namedTemporaryDir() as tmpdir:
+            manifest = make_blocksd_manifest(tmpdir)
+            lvm = FakeLVM(tmpdir)
+            vg_name = make_vg(lvm, manifest)
+            with MonkeyPatchScope([(blockSD, 'lvm', lvm)]):
+                # 512 is derived from calculations in the metaSize function.
+                # Our small volume group will use the minimum allowed size.
+                self.assertEquals(self.MINIMUM_METADATA_SIZE / MB,
+                                  manifest.metaSize(vg_name))
+                lvm.vgmd[vg_name]['free'] = self.MINIMUM_METADATA_SIZE - 1
+                self.assertRaises(se.VolumeGroupSizeError,
+                                  manifest.metaSize, vg_name)
+
+    def test_getmetadatamapping_bad_metadata_extent(self):
+        """
+        We require the metadata LV to reside on the first extent of the first
+        PV in the VG.  Check that we raise an error if this is not the case.
+        """
+        with namedTemporaryDir() as tmpdir:
+            manifest = make_blocksd_manifest(tmpdir)
+            lvm = FakeLVM(tmpdir)
+            devices = get_random_devices(1)
+            vg_name = make_vg(lvm, manifest, devices)
+
+            # Fake a bad first extent (1) for our 'metadata' LV
+            lvm.lvmd[vg_name][sd.METADATA]['devices'] = \
+                '/dev/mapper/{0}(1)'.format(devices[0])
+
+            with MonkeyPatchScope([(blockSD, 'lvm', lvm)]):
+                self.assertRaises(se.MetaDataMappingError,
+                                  manifest.getMetaDataMapping, vg_name)
+
+    def test_getmetadatamapping(self):
+        """
+        Validate the behavior of getMetadataMapping including:
+         - pestart is 0 for all PVs in the VG (we override the real value 129M)
+         - PVs' physical extents (pe) are mapped into the VG in the order that
+           the PVs are added to the VG.  The mapoffset field shows the pe
+           number in the VG map to which this PV's first pe maps.
+        """
+        with namedTemporaryDir() as tmpdir:
+            manifest = make_blocksd_manifest(tmpdir)
+            lvm = FakeLVM(tmpdir)
+            devices = get_random_devices(3)
+            vg_name = make_vg(lvm, manifest, devices)
+
+            with MonkeyPatchScope([(blockSD, 'lvm', lvm)]):
+                mapping = manifest.getMetaDataMapping(vg_name)
+                # The values below assume that each device in the VG is 10G
+                self.assertEquals(3, len(mapping))
+                self.assertEquals(0, mapping['PV0']['pestart'])
+                self.assertEquals(0, mapping['PV0']['mapoffset'])
+                self.assertEquals(0, mapping['PV1']['pestart'])
+                self.assertEquals(77, mapping['PV1']['mapoffset'])
+                self.assertEquals(0, mapping['PV2']['pestart'])
+                self.assertEquals(154, mapping['PV2']['mapoffset'])
+
+    def test_getmetadatamapping_with_oldinfo(self):
+        """
+        When a dictionary of cached mapping info is supplied to
+        getMetaDataMapping lvm will only be queried for devices which are not
+        in the cached info.  Verify this behavior.
+        """
+        devices = get_random_devices(3)
+        info = {'PV1': {'pestart': 0, 'mapoffset': 79, 'pecount': 79,
+                        'guid': name_to_guid(devices[1]),
+                        'uuid': '5CUMKd-yJDg-a1BY-Dhe1-llAo-Waxrr3'},
+                'PV0': {'pestart': 0, 'mapoffset': 0, 'pecount': 79,
+                        'guid': name_to_guid(devices[0]),
+                        'uuid': '3ApdDe-dN8D-i6ay-cr60-uu3B-BxCclJ'}}
+
+        with namedTemporaryDir() as tmpdir:
+            manifest = make_blocksd_manifest(tmpdir)
+            lvm = FakeLVM(tmpdir)
+            vg_name = make_vg(lvm, manifest, devices)
+
+            # Remove PV info from lvm to test that we rely on the info dict
+            for i in 0, 1:
+                del lvm.pvmd[devices[i]]
+
+            with MonkeyPatchScope([(blockSD, 'lvm', lvm)]):
+                mapping = manifest.getMetaDataMapping(vg_name, info)
+                self.assertEquals(3, len(mapping))
+                self.assertEquals(name_to_guid(devices[2]),
+                                  mapping['PV2']['guid'])
+                for pv in ('PV0', 'PV1'):
+                    self.assertEquals(info[pv], mapping[pv])
+
+    def test_extendvolume(self):
+        """
+        Test the positive flow for extendVolume making sure the size is updated
+        """
+        with namedTemporaryDir() as tmpdir:
+            manifest = make_blocksd_manifest(tmpdir)
+            lvm = FakeLVM(tmpdir)
+            vg_name = make_vg(lvm, manifest)
+            lv = str(uuid.uuid4())
+            lvm.createLV(vg_name, lv, VOLSIZE)
+            with MonkeyPatchScope([(blockSD, 'lvm', lvm)]):
+                self.assertEquals(VOLSIZE, manifest.getVSize('unused', lv))
+                manifest.extendVolume(lv, 2 * VOLSIZE)
+                self.assertEquals(2 * VOLSIZE, manifest.getVSize('unused', lv))
+
+    def test_extend_maxpvs(self):
+        """
+        You may not extend a VG with additional devices if the number of PVs
+        belonging to it would rise beyond an allowed limit.  Verify this check.
+        """
+        with namedTemporaryDir() as tmpdir:
+            metadata = FakeMetadata()
+            manifest = make_blocksd_manifest(tmpdir, metadata)
+            metadata[sd.DMDK_VERSION] = 0
+            lvm = FakeLVM(tmpdir)
+            nr_initial_devices = 5
+            devices = get_random_devices(nr_initial_devices)
+            make_vg(lvm, manifest, devices)
+
+            new_dev_count = blockSD.MAX_PVS - nr_initial_devices + 1
+            devices = get_random_devices(new_dev_count)
+            with MonkeyPatchScope([(blockSD, 'lvm', lvm),
+                                   (multipath, 'getMPDevNamesIter',
+                                    lambda: iter(devices))]):
+                # Part of the test setup requires that the domain metadata
+                # includes the PV mapping information for the original device
+                # list.  Rather than hard-coding it, it's easier to use
+                # updateMapping() which is designed for this purpose.
+                manifest.updateMapping()
+                self.assertRaises(se.StorageDomainIsMadeFromTooManyPVs,
+                                  manifest.extend, devices, False)
+
+    def test_extend_unknowndevs(self):
+        """
+        Devices which are not known to multipath cannot be used to extend a VG.
+        """
+        with namedTemporaryDir() as tmpdir:
+            metadata = FakeMetadata()
+            manifest = make_blocksd_manifest(tmpdir, metadata)
+            metadata[sd.DMDK_VERSION] = 3
+            lvm = FakeLVM(tmpdir)
+            devices = get_random_devices()
+            with MonkeyPatchScope([(blockSD, 'lvm', lvm),
+                                   (multipath, 'getMPDevNamesIter',
+                                    lambda: list())]):
+                self.assertRaises(se.InaccessiblePhysDev,
+                                  manifest.extend, devices, False)
+
+    def test_extend(self):
+        """
+        Test the positive flow for extend and verify the new size and mapping
+        """
+        with namedTemporaryDir() as tmpdir:
+            metadata = FakeMetadata()
+            manifest = make_blocksd_manifest(tmpdir, metadata)
+            metadata[sd.DMDK_VERSION] = 3
+            lvm = FakeLVM(tmpdir)
+            devices = get_random_devices(1)
+            vg_name = make_vg(lvm, manifest, devices)
+            new_devices = get_random_devices(1)
+            with MonkeyPatchScope([(blockSD, 'lvm', lvm),
+                                   (multipath, 'getMPDevNamesIter',
+                                    lambda: iter(new_devices))]):
+                manifest.updateMapping()
+                manifest.extend(new_devices, False)
+                newSize = 2 * (10 * GB - self.PV_UNUSABLE_SIZE)
+                # TODO: Switch to using manifest.getStats when it is ready
+                self.assertEquals(newSize, lvm.getVG(vg_name).size)
+                self.assertEquals(2, len(manifest.getMetaDataMapping(vg_name)))
+
+    def test_resizepv(self):
+        """
+        Test that resizePV handles device size update and metadata makes sense.
+        """
+        with namedTemporaryDir() as tmpdir:
+            manifest = make_blocksd_manifest(tmpdir)
+            lvm = FakeLVM(tmpdir)
+            devices = get_random_devices(1)
+            vg_name = make_vg(lvm, manifest, devices)
+            with MonkeyPatchScope([(blockSD, 'lvm', lvm)]):
+                # Set up the current mapping according to current device size
+                manifest.updateMapping()
+                pv = lvm.pvmd.values()[0]
+                lvm.fake_pv_update_size(pv['name'], 20 * GB)
+                manifest.resizePV(pv['guid'])
+                self.assertEquals(self.MINIMUM_METADATA_SIZE / MB,
+                                  manifest.metaSize(manifest.sdUUID))
+                self.assertEquals(self.MINIMUM_METADATA_SIZE / MB,
+                                  lvm.getLV(vg_name, sd.METADATA).size)
+                newSize = 20 * GB - self.PV_UNUSABLE_SIZE
+                self.assertEquals(newSize, lvm.getVG(vg_name).size)
+
+
+def name_to_guid(name):
+    return os.path.basename(name)
--- tests/storagefakelib.py
+++ tests/storagefakelib.py
@@ -26,19 +26,24 @@ from storage import lvm as real_lvm
 
 
 class FakeLVM(object):
-    _PV_SIZE = 10 << 30  # We pretend all PVs contribute 10G of space
+    _PV_SIZE = 10 << 30             # We pretend all PVs are 10G in size
+    _PV_PE_SIZE = 128 << 20         # Found via inspection of real environment
+    _PV_UNUSABLE = _PV_PE_SIZE * 3  # 2 PE for metadata + 1 PE to hold a header
 
     def __init__(self, root):
         self.root = root
         os.mkdir(os.path.join(self.root, 'dev'))
+        self.pvmd = {}
         self.vgmd = {}
         self.lvmd = {}
 
     def createVG(self, vgName, devices, initialTag, metadataSize,
                  extentsize=134217728, force=False):
         devices = [_fqpvname(dev) for dev in devices]
-        size = self._PV_SIZE * len(devices)
-        extent_count = size / extentsize
+        for dev in devices:
+            self._create_pv(dev, vgName, self._PV_SIZE)
+        extent_count = self._calc_vg_pe_count(vgName)
+        size = extent_count * self._PV_PE_SIZE
 
         vg_attr = dict(permission='w',
                        resizeable='z',
@@ -57,12 +62,26 @@ class FakeLVM(object):
                      tags=[initialTag],
                      vg_mda_size=metadataSize,
                      vg_mda_free=metadataSize,
-                     lv_count='0',
+                     lv_count=0,
                      pv_count=len(devices),
                      pv_name=tuple(devices),
                      writeable=True,
                      partial='OK')
         self.vgmd[vgName] = vg_md
+
+    def extendVG(self, vgName, devices, force):
+        md = self.vgmd[vgName]
+        old_pe_count = self._calc_vg_pe_count(vgName)
+        devices = [_fqpvname(dev) for dev in devices]
+        for dev in devices:
+            self._create_pv(dev, vgName, self._PV_SIZE)
+        nr_pe_added = self._calc_vg_pe_count(vgName) - old_pe_count
+        md['pv_count'] += len(devices)
+        md['pv_name'] = tuple(md['pv_name'] + tuple(devices))
+        md['size'] += nr_pe_added * self._PV_PE_SIZE
+        md['free'] += nr_pe_added * self._PV_PE_SIZE
+        md['extent_count'] += nr_pe_added
+        md['free_count'] += nr_pe_added
 
     def createLV(self, vgName, lvName, size, activate=True, contiguous=False,
                  initialTags=[]):
@@ -88,9 +107,20 @@ class FakeLVM(object):
 
         vg_dict = self.lvmd.setdefault(vgName, {})
         vg_dict[lvName] = lv_md
+        self.vgmd[vgName]['lv_count'] += 1
+
+    def extendLV(self, vgName, lvName, size):
+        # TODO: Update the allocated physical extents
+        self.lvmd[vgName][lvName]['size'] = size
 
     def lvPath(self, vgName, lvName):
         return os.path.join(self.root, "dev", vgName, lvName)
+
+    def getPV(self, pvName):
+        md = deepcopy(self.pvmd[pvName])
+        vg_name = md['vg_name']
+        md['vg_uuid'] = self.vgmd[vg_name]['uuid']
+        return real_lvm.PV(**md)
 
     def getVG(self, vgName):
         vg_md = deepcopy(self.vgmd[vgName])
@@ -104,11 +134,80 @@ class FakeLVM(object):
         lv_md['attr'] = lv_attr
         return real_lvm.LV(**lv_md)
 
+    def getFirstExt(self, vg, lv):
+        """
+        Based on the following example output from real LVM:
+        # lvs -o name,devices
+        LV                                   Devices
+        4c705e28-ad99-46c2-b1eb-43fd8501ea97 /dev/mapper/1IET_00010004(31)
+        ids                                  /dev/mapper/1IET_00010004(21)
+        inbox                                /dev/mapper/1IET_00010004(22)
+        leases                               /dev/mapper/1IET_00010004(5)
+        master                               /dev/mapper/1IET_00010004(23)
+        metadata                             /dev/mapper/1IET_00010004(0)
+        outbox                               /dev/mapper/1IET_00010004(4)
+        """
+        return self.getLV(vg, lv).devices.strip(" )").split("(")
+
+    def listPVNames(self, vgName):
+        return self.getVG(vgName).pv_name
+
     def fake_lv_symlink_create(self, vg_name, lv_name):
         volpath = self.lvPath(vg_name, lv_name)
         os.makedirs(os.path.dirname(volpath))
         with open(volpath, "w") as f:
             f.truncate(int(self.lvmd[vg_name][lv_name]['size']))
+
+    def _create_pv(self, pv_name, vg_name, size):
+        """
+        pe_start is set based on the following example from real LVM:
+        # vgs --units b -o pv_name,pe_start,pv_pe_count,pv_pe_alloc_count,\
+        pv_size 2e133cd6-bfea-4655-ae6c-dc51a38bb7b9
+        PV                        1st PE     PE  Alloc PSize
+        /dev/mapper/1IET_00070001 135266304B 397    65 53284438016B
+        /dev/mapper/1IET_00070002 135266304B 397     0 53284438016B
+
+        This is an example vg with 2 pvs and 10 lvs.
+        """
+        pe_start = 135266304
+        pe_count = (size - self._PV_UNUSABLE) / self._PV_PE_SIZE
+        pv_md = dict(uuid=fake_lvm_uuid(),
+                     name=pv_name,
+                     guid=os.path.basename(pv_name),
+                     size=self._PV_SIZE,
+                     vg_name=vg_name,
+                     vg_uuid=None,
+                     pe_start=pe_start,
+                     pe_count=pe_count,
+                     pe_alloc_count=0,
+                     mda_count=None,
+                     dev_size=self._PV_SIZE)
+        self.pvmd[pv_name] = pv_md
+
+    def fake_pv_update_size(self, pv_name, new_size):
+        pvmd = self.pvmd[pv_name]
+        size_added = new_size - pvmd['size']
+        extents_added = size_added / self._PV_PE_SIZE
+        pvmd['size'] += size_added
+        pvmd['dev_size'] += size_added
+        pvmd['pe_count'] += extents_added
+
+        vg_name = pvmd['vg_name']
+        vgmd = self.vgmd[vg_name]
+        vgmd['size'] += size_added
+        vgmd['free'] += size_added
+        vgmd['extent_count'] += extents_added
+        vgmd['free_count'] += extents_added
+
+    def resizePV(self, vg_name, guid):
+        # There is nothing to do since fake_pv_update_size() took care of
+        # the LVM synchronization.
+        pass
+
+    def _calc_vg_pe_count(self, vg_name):
+        return sum(pv["pe_count"] for pv in self.pvmd.values()
+                   if pv["vg_name"] == vg_name)
+
 
 _fqpvname = real_lvm._fqpvname
 
